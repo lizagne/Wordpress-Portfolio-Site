1e475bdbac4f8d6bf8d867a76fd361cf0be78102
Liz Verde <>
2017-12-30 17:19:43 +0000

    Created a new file called page-home.php, this will be the template for the Home page. This then contains all the content HTML code from my static HTML page

--- footer.php
+++ footer.php
@@ -12,6 +12,66 @@
 ?>
 <?php wp_footer(); ?>
 
+<!-- FOOTER
+	================================================== -->
+	<footer>
+		<div class="container">
+			<div class="col-sm-3">
+				<p><a href="/"><img src="<?php bloginfo('template_directory'); ?>/assets/img/logo.png" alt="Liz Verde, Junior Web Developer"></a></p>
+			</div><!-- end col -->
+			<div class="col-sm-6">
+				<nav>
+					<ul class="list-unstyled list-inline">
+						<li><a href="">Home</a></li>
+						<li><a href="">Projects</a></li>
+						<li><a href="">Skills</a></li>
+						<li><a href="">About</a></li>
+						<li><a href="">Contact</a></li>
+					</ul>
+				</nav>
+			</div><!-- end col -->
+			<div class="col-sm-3">
+				<p class="pull-right">&copy; 2018 Liz Verde</p>
+			</div><!-- end col -->
+		</div><!-- container -->
+	</footer>
+
+
+	<!-- MODAL
+	================================================== -->
+	<div class="modal fade" id="myModal">
+		<div class="modal-dialog">
+			<div class="modal-content">
+			
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+					<h4 class="modal-title" id="myModalLabel"><i class="fa fa-envelope"></i> Subscribe to our Mailing List</h4>
+				</div><!-- modal-header -->
+				
+				<div class="modal-body">
+					<p>Simply enter your name and email! As a thank you for joining us, we're going to give you one of our best-selling courses, <em>for free!</em></p>
+					
+					<form class="form-inline" role="form">
+						<div class="form-group">
+							<label class="sr-only" for="subscribe-name">Your first name</label>
+							<input type="text" class="form-control" id="subscribe-name" placeholder="Your first name">
+						</div>
+						<div class="form-group">
+							<label class="sr-only" for="subscribe-email">and your email</label>
+							<input type="text" class="form-control" id="subscribe-email" placeholder="and your email">
+						</div>
+						<input type="submit" class="btn btn-danger" value="Subscribe!">
+					</form>
+					
+					<hr>
+					
+					<p><small>By providing your email you consent to receiving occasional promotional emails &amp; newsletters. <br>No Spam. Just good stuff. We respect your privacy &amp; you may unsubscribe at any time.</small></p>
+				</div><!-- modal-body -->
+				
+			</div><!-- modal-content -->
+		</div><!-- modal-dialog -->
+	</div><!-- modal -->
+
 <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
---
+++ page-home.php
@@ -0,0 +1,217 @@
+<?php
+/*
+
+	Template Name: Home Page
+
+
+ */
+
+get_header(); ?>
+
+	   <!-- HERO
+    ================================================== -->
+    <section id="hero" data-type="background" data-speed="5">
+    	<article>
+    		<div class="container clearfix">
+    			<div class="row">
+    			
+    				<div class="col-sm-5">
+		    			<img src="/wp-content/themes/underscoresTheme/assets/img/lizVerde.jpg" alt="Liz Verde, Junior Web Developer" class="logo">
+    				</div><!-- col -->
+		    		
+		    		<div class="col-sm-7 hero-text">
+			    		<h1>//Junior Web Developer</h1>
+			            <p class="lead">Since making the decision to study programming I have become addicted to learning the languages that make up digital experiences. I work very hard at improving my skills, through repeated practice and online courses.</p>
+
+			            <p><a class="btn btn-lg btn-danger" href="#projects" role="button">//See My Work &raquo;</a></p>
+		    		</div><!-- col -->
+		    		
+    			</div><!-- row -->
+    		</div><!-- container -->
+    	</article>
+    </section>
+	
+   <!-- PROJECTS
+	================================================== -->
+	<section id="projects">
+		<div class="container">
+			<a name="projects"></a>
+			<h2>//My Projects</h2>
+			<p class="lead">During our Coding Fellowship with DevelopMe_, we worked on various different projects with lots of different technologies. My favourite subjects have been React, React Native and Redux. I've been working hard at trying to fully understand these subjects, and although have struggled a lot, enjoy them hugely.</p> 
+			<p><strong>Take a look at some of my projects, here and on Github</strong></p>
+			
+			<div class="row">
+				<div class="col-sm-4">
+					<img src="/wp-content/themes/underscoresTheme/assets/img/icon-design.png" alt="Design">
+					<h3>React Native</h3>
+					<p>Week 9 of the Coding Fellowship we got to make various different mobile apps. Take a look here</p>
+				</div><!-- col -->
+				<div class="col-sm-4">
+					<img src="/wp-content/themes/underscoresTheme/assets/img/icon-code.png" alt="Code">
+					<h3>Quality HTML5 &amp; CSS3</h3>
+					<p>You'll learn how hand-craft a stunning website with valid, semantic and beautiful HTML5 &amp; CSS3.</p>
+				</div><!-- col -->
+				<div class="col-sm-4">
+					<img src="/wp-content/themes/underscoresTheme/assets/img/icon-cms.png" alt="CMS">
+					<h3>Easy-to-use CMS</h3>
+					<p>Allow your clients to easily update their websites by converting your static websites to dynamic websites, using WordPress.</p>
+				</div><!-- col -->
+				<div class="col-sm-4">
+					<img src="/wp-content/themes/underscoresTheme/assets/img/icon-design.png" alt="Design">
+					<h3>React Native</h3>
+					<p>Week 9 of the Coding Fellowship we got to make various different mobile apps. Take a look here</p>
+				</div><!-- col -->
+				<div class="col-sm-4">
+					<img src="/wp-content/themes/underscoresTheme/assets/img/icon-code.png" alt="Code">
+					<h3>Quality HTML5 &amp; CSS3</h3>
+					<p>You'll learn how hand-craft a stunning website with valid, semantic and beautiful HTML5 &amp; CSS3.</p>
+				</div><!-- col -->
+				<div class="col-sm-4">
+					<img src="/wp-content/themes/underscoresTheme/assets/img/icon-cms.png" alt="CMS">
+					<h3>Easy-to-use CMS</h3>
+					<p>Allow your clients to easily update their websites by converting your static websites to dynamic websites, using WordPress.</p>
+				</div><!-- col -->
+				
+			</div><!-- row -->
+			
+		</div><!-- container -->
+	</section><!-- project-features -->
+
+	<!-- SKILLS
+	================================================== -->
+	<section id="skills">
+		<div class="container">
+			<a name="skills"></a>
+			<div class="section-header">
+				<h2>//Skills and Courses</h2>
+			</div><!-- section-header -->
+			
+			<div class="row">
+				
+				<div class="col-sm-2">
+					<i class="fa fa-html5" ></i>
+					<p>HTML 5 // CSS // SASS</p>
+				</div><!-- end col -->
+				
+				<div class="col-sm-2">
+					<i class="fa fa-css3" aria-hidden="true"></i>
+					<p>JavaScript // jQuery // JSX </p>
+				</div><!-- end col -->
+				
+				<div class="col-sm-2">
+					<i class="ci ci-calendar"></i>
+					<p>React // React Native // Redux</p>
+				</div><!-- end col -->
+				
+				<div class="col-sm-2">
+					<i class="ci ci-community"></i>
+					<p>PHP // MySQL // Laravel</p>
+				</div><!-- end col -->
+				
+				<div class="col-sm-2">
+					<i class="ci ci-instructor"></i>
+					<p> Wordpress // Drupal</p>
+				</div><!-- end col -->
+				
+				<div class="col-sm-2">
+					<i class="ci ci-device"></i>
+					<p>Git // Github // Command Line // Postman // Node // Yarn </p>
+				</div><!-- end col -->
+				
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- course-features -->
+
+	<!-- VIDEO FEATURETTE
+	================================================== -->
+	<a name="codepen"></a>
+	<section id="codepen">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2">
+					<h2>//See my pens on Codepen</h2>
+					<p>In November this year, I participated in Codevember, which is a challenge for developers making a creative digital sketch by day during the whole month of November. Inspired by Inktober, which I'd participated in the previous 2 years, in my life as an Artist. I decided to take on this challenge on Codepen. I kept it up every day, until we came to React, where I needed all the same time I had to try and figure out the weird and wonderful world of Apps!</p> 
+				</div><!-- end col -->
+			</div><!-- row -->			
+		</div><!-- container -->
+	</section><!-- featurette -->
+
+	<!-- ABOUT SECTION
+	================================================== -->
+	<section id="about">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8 col-md-6 col-lg-7">
+					<div class="row">
+						<div class="col-lg-8">
+							<h2>//About</h2>
+							<p class="lead">Liz Verde<small> Junior Web Developer</small></p>
+						</div><!-- end col -->
+						<div class="col-lg-4">
+							<a href="https://twitter.com/zizdrawstoo" class="badge social twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+							<a href="https://github.com/lizagne" class="badge social github" target="_blank"><i class="fa fa-github"></i></a>
+							<a href="https://codepen.io/lizagne" class="badge social codepen" target="_blank"><i class="fa fa-codepen"></i></a>
+						</div><!-- end col -->
+					
+					</div><!-- row -->
+					
+					<p class="lead">I am passionate about learning new technologies that further my understanding of all things web.<p>
+					
+					<p>My background is in Graphic Design and Art, and having worked alongside Developers, I had long felt that it was something that was totally separate. Then this year I worked at Dyson on a temporary contract helping to put together their new website. There I worked alongside Developers who had previously been on a bootcamp. My role at Dyson was very limited, whereas the Developers had all the control and all the fun. I looked into the Coding Fellowship course here and since working through tasks on FreeCode Camp, Codecademy and Treehouse, I've been addicted to learning more and more. I've struggled with some subjects, but it challenges me further to put the hours in and with repeated practice, things have become to click.</p>
+					
+					<hr>
+
+				</div><!-- end col -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- instructor -->
+
+	
+	
+	<!-- CONTACT SECTION 
+	================================================== -->
+	<a name="contact"></a>
+	<section id="contact" data-type="background" data-speed="4">
+		<div class="container">
+		    <div class="row" id="primary">
+			    <div id="content" class="col-sm-12">    
+				    <section class="main-content">
+				    	<h2>//Contact</h2>
+				    	<p class="lead">07813 791 655  •  ljverde@gmail.com</p>
+				    	
+				    	<form role="form" class="clearfix">
+		    				<div class="row">
+		    					<div class="col-sm-6">
+		    						<div class="form-group">
+			    						<label class="sr-only" for="contact-name">Name</label>
+										<input type="text" class="form-control input-lg" id="contact-name" placeholder="Your name">
+		    						</div>
+		    					</div><!-- end col -->
+		    					
+		    					<div class="col-sm-6">
+		    						<div class="form-group">
+			    						<label class="sr-only" for="contact-email">Email</label>
+										<input type="text" class="form-control input-lg" id="contact-email" placeholder="Your email">
+		    						</div>
+		    					</div><!-- end col -->
+		    					
+		    					<div class="col-sm-12">
+		    						<div class="form-group">
+			    						<label class="sr-only" for="contact-words">Message</label>
+			    						<textarea class="form-conrol input-lg" id="contact-words" placeholder="Your message" rows="3"></textarea>
+		    						</div>
+		    					</div><!-- end col -->
+		    				</div><!-- row -->
+							<input type="submit" class="btn btn-info btn-lg pull-right" value="//Get in touch &raquo;">
+				        </form>
+				    	
+				    </section>
+			    	
+			    </div><!-- content -->
+			    	    
+		    </div><!-- primary -->
+		</div><!-- container -->
+	</section><!-- signup -->
+
+<?php
+get_footer(); ?>
