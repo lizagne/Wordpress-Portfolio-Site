ec9acadc1a2c73c3ead2c2216f68a74d5290c76c
Liz Verde <>
2017-12-30 15:46:11 +0000

    Changing the screenshot for my theme so that it's easily recognisable in the Themes section of Wordpress

--- header.php
+++ header.php
@@ -59,7 +59,7 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="/"><img src="assets/img/logo.png" alt="Liz Verde, Junior Web Developer"></a>
+						<a class="navbar-brand" href="/"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo.png" alt="Liz Verde, Junior Web Developer"></a>
 					</div> <!-- navbar header -->
 
 					<?php
