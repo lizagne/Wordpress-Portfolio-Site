166a69eb48d258ef1195268a4522fc5e189eec04
Liz Verde <>
2017-12-30 18:46:10 +0000

    Updating header and page template to work out what is going wrong with menu bar

--- header.php
+++ header.php
@@ -1,47 +1,43 @@
 <?php
 /**
- * The header for our theme
+ * The header for our theme.
  *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
+ * Displays all of the <head> section and everything up till <div id="content">
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Bootstrap_to_Wordpress
+ * @package Bootstrap to WordPress
  */
-
-?>
-<!doctype html>
+?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+	
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<!-- Bootstrap core CSS -->
 	<link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
 	
 	<!-- Font Awesome Icons -->
 	<link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-
+	
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Arvo|Bree+Serif|Josefin+Sans|Lora" rel="stylesheet">
 
-	<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
+<?php wp_head(); ?>
+
+<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
-	<?php wp_head(); ?>
+<![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bootstrap2wordpress' ); ?></a>
-
-
-
-
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'underscoresTheme' ); ?></a>
+	
 	<!-- HEADER
 	================================================== -->
 	<header class="site-header" role="banner">
@@ -51,6 +47,7 @@
 		<div class="navbar-wrapper">
 			
 			<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+				
 				<div class="container">
 					<div class="navbar-header">
 						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -59,35 +56,28 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="/"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo.png" alt="Liz Verde, Junior Web Developer"></a>
-					</div> <!-- navbar header -->
-
+						
+						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo.png" alt="Bootstrap to WordPress"></a>
+						
+					</div><!-- navbar-header -->
+					
+					<!-- If the menu (WP admin area) is not set, then the "menu_class" is applied to "container". In other words, it overwrites the "container_class". Ref: http://wordpress.org/support/topic/wp_nav_menu-menu_class-usage-bug?replies=4 -->
+					
 					<?php
-
 						wp_nav_menu( array(
-
-							'theme_location' => 'primary',
-							'container' 	=> 'nav',
-							'container_class' => 'navbar-collapse collapse',
-							'menu_class' => 'nav navbar-nav navbar-right'
-
-						));
-
-
+							
+							'theme_location'	=> 'primary',
+							'container'			=> 'nav',
+							'container_class'	=> 'navbar-collapse collapse',
+							'menu_class'		=> 'nav navbar-nav navbar-right'
+							
+						) );
 					?>
-
-
-					<!-- <div class="navbar-collapse collapse">
-						<ul class="nav navbar-nav navbar-right">
-							<li class="active"><a href="/">Home</a></li>
-							<li><a href="#projects">Projects</a></li>
-							<li><a href="#skills">Skills</a></li>
-							<li><a href="#about">About</a></li>
-							<li><a href="#contact">Contact</a></li>
-						</ul>
-					</div> -->
-				</div>
-			</div>
+					
+				</div><!-- container -->
+				
+			</div><!-- navbar -->
+			
+		</div><!-- navbar-wrapper -->
 		
-		</div>
 	</header>
--- page-home.php
+++ page-home.php
@@ -3,7 +3,6 @@
 
 	Template Name: Home Page
 
-
  */
 
 get_header(); ?>
